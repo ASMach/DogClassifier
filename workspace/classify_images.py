7305d0c76a5c1fa289e60367474752d637423c31
Alonzo Machiraju <machthree@gmail.com>
2021-09-01 10:44:11 -0700

    Update classify_images.py
    Fixed replacement of append with extend

--- workspace/classify_images.py
+++ workspace/classify_images.py
@@ -102,7 +102,7 @@ def classify_images(images_dir, results_dic, model):
        # as an exact match to on of the terms in the list - then they are added to 
        # results_dic as an exact match(1) using extend list function
        if truth in model_label:
-           results_dic[key].append(1)
+           results_dic[key].extend([model_label, 1])
 
        # TODO: 3d. REPLACE pass BELOW with CODE that uses the extend list function
        #           to add the classifier label (model_label) and the value of
@@ -113,4 +113,4 @@ def classify_images(images_dir, results_dic, model):
        # if not found then added to results dictionary as NOT a match(0) using
        # the extend function 
        else:
-           results_dic[key].append(0)
+           results_dic[key].extend([model_label, 0])
